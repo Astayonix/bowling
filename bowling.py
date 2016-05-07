e778bd23b1643577689f45c53a2ec111a4a24f92
Astayonix <jessica.l.burns@gmail.com>
2016-05-07 12:02:33 -0700

    Added first unit test

--- bowling.py
+++ bowling.py
@@ -1,6 +1,6 @@
 from random import choice
 
-class BowlingAlley(object):
+class BowlingGame(object):
 
     lane = range(10)
     frames = 10
@@ -11,16 +11,15 @@ class BowlingAlley(object):
     def __init__(self, num_players, bumpers):
         self.num_players = num_players
         self.bumpers = bumpers
-        # pins = range(10)
+
     def showLane(self):
+        """Shows the current configuration of the pins in the lane"""
         print "||  %s %s %s %s  ||" % (self.lane[0],self.lane[1],self.lane[2],self.lane[3])
         print "||   %s %s %s   ||" % (self.lane[4], self.lane[5], self.lane[6])
         print "||    %s %s    ||" % (self.lane[7], self.lane[8])
         print "||     %s     ||" % (self.lane[9])
 
-myGame = BowlingAlley(3, True)
+myGame = BowlingGame(3, True)
 
-# print myGame.pins
-# print myGame.frames
 print myGame.showLane()
-print myGame.bumpers
+# print myGame.bumpers
--- test_bowling.py
+++ test_bowling.py
@@ -1,5 +1,13 @@
 import unittest
 import bowling
 
-class bowlingunittests(unittest.TestCase):
-    pass
+class BowlingGameUnitTests(unittest.TestCase):
+
+    def test_startGame(self):
+        myGame = bowling.BowlingGame(3, True)
+    
+    # def test_showLane(self):
+    #     assert()
+
+if __name__ == "__main__":
+    unittest.main()
